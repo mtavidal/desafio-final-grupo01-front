667b2c7433e4e16f0ec174a15bc062dc012e4e78
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-06-01 16:42:45 -0300

    adiciona arquivo Router, ajusta rotas, adiciona pasta lib e instala axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
 import Cabecalho from "componentes/Cabecalho";
 import Rodape from "componentes/Rodape";
-import Carrinho from "paginas/Carrinho";
-import Home from "paginas/Home";
-import ListagemProdutos from "paginas/ListagemProdutos";
-import Login from "paginas/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Router from "Router";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Cabecalho />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/produtos" element={<ListagemProdutos />} />
-        <Route path="/carrinho" element={<Carrinho />} />
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/" element={<PaginaBase />}>
-            <Route index element={<Inicio />} />
-            <Route path="Favoritos" element={<Favoritos />} />
-            <Route path=":id" element={<Player />} />
-            <Route path="*" element={<NaoEncontrada />} />
-        </Route> */}
-      </Routes>
+      <Router />
       <Rodape />
-    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Router.tsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
+import Carrinho from "paginas/Carrinho";
+import Home from "paginas/Home";
+import ListagemProdutos from "paginas/ListagemProdutos";
+import Login from "paginas/Login";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/produtos" element={<ListagemProdutos />} />
+      <Route path="/carrinho" element={<Carrinho />} />
+      <Route path="/login" element={<Login />} />
+      {/* <Route path="/" element={<PaginaBase />}>
+            <Route index element={<Inicio />} />
+            <Route path="Favoritos" element={<Favoritos />} />
+            <Route path=":id" element={<Player />} />
+            <Route path="*" element={<NaoEncontrada />} />
+        </Route> */}
+    </Routes>
+  );
+}
