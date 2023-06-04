a2c6d6a7597abc7fd015ef366fb56d8211a1bff8
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-06-04 19:57:01 -0300

    Adiciona componente Banner carrossel e detalhes pagina Home

---
+++ src/componentes/CarrosselBanner/index.js
@@ -0,0 +1,30 @@
+import Carousel from "nuka-carousel"
+import styles from "./CarroselBanner.module.css"
+import imagem1 from "./imagensBanner/imagem1.jpg"
+import imagem2 from "./imagensBanner/Imagem2.jpg"
+import imagem3 from "./imagensBanner/Imagem3.jpg"
+import imagem4 from "./imagensBanner/Imagem4.jpg"
+import imagem5 from "./imagensBanner/imagem5.jpg"
+
+export default function CarrosselBanner() {
+    return (
+        <Carousel autoplay={true} autoplayInterval={2500} className={styles.carrossel} withoutControls={true} wrapAround={true} animation={"fade"} speed={1000} >
+            <div>
+                <img src={imagem1} alt="oculos" />
+            </div>
+            <div>
+                <img src={imagem2} alt="oculos" />
+            </div>
+            <div>
+                <img src={imagem3} alt="oculos" />
+            </div>
+            <div>
+                <img src={imagem4} alt="oculos" />
+            </div>
+            <div>
+                <img src={imagem5} alt="oculos" />
+            </div>
+
+        </Carousel >
+    )
+}
--- src/paginas/Home/index.tsx
+++ src/paginas/Home/index.tsx
@@ -2,6 +2,12 @@ import { ListarProdutos } from "componentes/ListarProdutos";
 import styles from "./Home.module.css";
 import { useNavigate } from "react-router-dom";
 import Botao from "componentes/Botao";
+import CarrosselBanner from "componentes/CarrosselBanner";
+import logoCarrera from "./imagens/CARRERA-LOGO.png";
+import logoDior from "./imagens/LOGO-DIOR.png";
+import logoOakley from "./imagens/LOGO-OAKLEY.png";
+import logoRayban from "./imagens/LOGO-RAYBAN.png";
+import logoVogue from "./imagens/VOGUE-LOGO.png";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -11,7 +17,18 @@ export default function Home() {
   }
   return (
     <div className={styles.containerHome}>
-      <h1>Pagina Home</h1>
+      <h1 className={styles.titulo}>
+        Aqui você encontra as melhores marcas de óculos com os melhores preços
+        do Brasil!
+      </h1>
+      <CarrosselBanner />
+      <div className={styles.containerMarcas}>
+        <img src={logoCarrera} alt="logo carrera" />
+        <img src={logoDior} alt="logo dior" />
+        <img src={logoOakley} alt="logo oakley" />
+        <img src={logoRayban} alt="logo rayban" />
+        <img src={logoVogue} alt="logo vogue" />
+      </div>
       <ListarProdutos ehPaginaHome={true} limitPaginas={5} />
       <Botao onClick={irParaProdutos}>Ir para produtos</Botao>
     </div>
